8499b8e6eaede7bf64a95e290ed964e7c438ce41
claus <claus.z@protonmail.com>
2018-06-30 22:08:01 +1000

    Add CronLib

--- TemperatureTracker/TemperatureRunner.cs
+++ TemperatureTracker/TemperatureRunner.cs
@@ -5,29 +5,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Emmellsoft.IoT.Rpi.SenseHat;
+using CronLib;
 
 namespace TemperatureTracker
 {
+    public class TemperatureJob : CronJob
+    {
+        private readonly TimeSpan begin = new TimeSpan(7, 15, 0);
+        private readonly TimeSpan end = new TimeSpan(23, 15, 0);
+
+        private bool isInTime()
+        {
+            TimeSpan now = DateTime.Now.TimeOfDay;
+            return now > begin && now < end;
+        }
+
+        protected override async Task Functionality()
+        {
+            await Task.Run(async () =>
+            {
+                var senseHat = await SenseHatFactory.GetSenseHat().ConfigureAwait(false);
+                var wrap = new SenseHatWrapper(senseHat);
+                wrap.ClearDisplay();
+                var temperature = wrap.Temperature();
+                if (isInTime())
+                {
+                    wrap.Write(temperature.ToString(), TemperatureColourMap.colourForTemperature(temperature), DisplayDirection.Deg180);
+                }
+                wrap.Sleep(TimeSpan.FromSeconds(15));
+                wrap.ClearDisplay();
+            }).ConfigureAwait(false);
+        }
+    }
     public static class TemperatureRunner
     {
         public static void Run()
         {
-            Task.Run(async () =>
-            {
-                while (true)
-                {
-                    Debug.WriteLine("Start running");
-                    var senseHat = await SenseHatFactory.GetSenseHat().ConfigureAwait(false);
-                    Debug.WriteLine("Got a head");
-                    var wrap = new SenseHatWrapper(senseHat);
-                    wrap.ClearDisplay();
-                    var temperature = wrap.Temperature();
-                    wrap.Write(temperature.ToString(), TemperatureColourMap.colourForTemperature(temperature), DisplayDirection.Deg180);
-                    wrap.Sleep(TimeSpan.FromSeconds(5));
-                    wrap.ClearDisplay();
-                    wrap.Sleep(TimeSpan.FromSeconds(25));
-                }
-            }).ConfigureAwait(false);
+            CronRunner<TemperatureJob>.Run("0 0/1 * * * ?").GetAwaiter().GetResult();
         }
     }
 }
