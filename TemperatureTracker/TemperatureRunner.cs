40b8efe73107cefdc2933d7882654ef3034ead0e
claus <claus.z@protonmail.com>
2018-07-14 21:42:13 +1000

    Move TemperatureJob into its own class

--- TemperatureTracker/TemperatureRunner.cs
+++ TemperatureTracker/TemperatureRunner.cs
@@ -3,26 +3,10 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using CronLib;
 
 namespace TemperatureTracker
 {
-    public class TemperatureJob<TheSensor, TheWriter> : CronJob 
-        where TheSensor : ISensor, new() 
-        where TheWriter : ITemperatureWriter, new()
-    {
-        protected override async Task Functionality()
-        {
-            await Task.Run(async () =>
-            {
-                var wrap = new TheSensor();
-                var temperature = wrap.Temperature;
-                ITemperatureWriter writer = new TheWriter();
-                await writer.Write(temperature.ToString());
-            }).ConfigureAwait(false);
-        }
-    }
     public static class TemperatureRunner
     {
         public static void Run()
